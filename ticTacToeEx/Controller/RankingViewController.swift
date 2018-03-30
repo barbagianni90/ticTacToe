a7ce90ef29b2be8da92db47b08cf23328563cdf7
Stefano Apuzzo <stefanoapuzzo@macbook-pro-di-stefano.local>
2018-03-30 09:33:37 +0200

    ranking commit

--- ticTacToeEx/Controller/RankingViewController.swift
+++ ticTacToeEx/Controller/RankingViewController.swift
@@ -9,7 +9,28 @@
 import Foundation
 import UIKit
 
-class RankingViewController: UIViewController {
+class RankingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var vittorieLabel: UILabel!
+    @IBOutlet weak var giocatoreLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellRank")
+        
+        return cell!
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 }
