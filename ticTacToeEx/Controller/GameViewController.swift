9eaaa07688f45d50cf11a405f15515d43e83a1b4
Stefano Apuzzo <>
2018-03-30 10:54:40 +0200

    commit

--- ticTacToeEx/Controller/GameViewController.swift
+++ ticTacToeEx/Controller/GameViewController.swift
@@ -11,6 +11,28 @@ import UIKit
 
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var quartoBut: UIButton!
+    @IBOutlet weak var terzoBut: UIButton!
+    
+    @IBOutlet weak var secondBut: UIButton!
+    @IBOutlet weak var primoBut: UIButton!
+    
+    @IBAction func crossBut(_ sender: Any) {
+        
+        let image = UIImage(named: "cross")
+        primoBut.setImage(image, for: .normal)
+        secondBut.setImage(image, for: .normal)
+        
+    }
+    
+    @IBAction func naughtBut(_ sender: Any) {
+        
+        let image = UIImage(named: "cerchio")
+        terzoBut.setImage(image, for: .normal)
+        quartoBut.setImage(image, for: .normal)
+
+    }
+    
     @IBAction func button(_ sender: Any) {
         
         dismiss(animated: true, completion: nil)
