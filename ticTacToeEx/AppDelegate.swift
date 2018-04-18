6f254bd0fac59889073b485a7b099799361c67cd
CertimeterGroup <acostafacundo.95@gmail.com>
2018-04-18 12:25:27 +0200

    c

--- ticTacToeEx/AppDelegate.swift
+++ ticTacToeEx/AppDelegate.swift
@@ -11,15 +11,14 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         // Override point for customization after application launch.
         
-    
         
         return true
     }
