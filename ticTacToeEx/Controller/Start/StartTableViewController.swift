41a4c0335ed9cb34454b1bc426bcc23019ce9f23
CertimeterGroup <acostafacundo.95@gmail.com>
2018-04-24 15:09:13 +0200

    logOut in start

--- ticTacToeEx/Controller/Start/StartTableViewController.swift
+++ ticTacToeEx/Controller/Start/StartTableViewController.swift
@@ -81,6 +81,7 @@ class StartTableViewController: UITableViewController {
                     ref.child("Players").child("\(MainViewController.user.id)").child("loggato").setValue("No")
                     MainViewController.user = User()
                     label.text = "Sign In"
+                    
                     print("Utente disconnesso\n")
                     
                     
